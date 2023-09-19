e642ddace930ea645399e68d1076dc716f16d94d
Hezron Na <hezron.w12@gmail.com>
2023-09-19 23:31:32 +0300

    Committed 2023 0Tu.233132

--- test3.py
+++ test3.py
@@ -1,17 +1,59 @@
 import webview
 import time
+"""
+This example demonstrates how to handle pywebview events.
+"""
 
-def toggle_fullscreen(window):
-    # wait a few seconds before toggle fullscreen:
-    time.sleep(5)
+def on_closed():
+    print('pywebview window is closed')
 
-    window.toggle_fullscreen()
+
+def on_closing():
+    print('pywebview window is closing')
+
+
+def on_shown():
+    print('pywebview window shown')
+
+
+def on_minimized():
+    print('pywebview window minimized')
+
+
+def on_restored():
+    print('pywebview window restored')
+
+
+def on_maximized():
+    print('pywebview window maximized')
+
+
+
+
+
+def on_resized(width, height):
+    print('pywebview window is resized. new dimensions are {width} x {height}'.format(width=width, height=height))
+
+
+def on_moved(x, y):
+    print('pywebview window is moved. new coordinates are x: {x}, y: {y}'.format(x=x, y=y))
 
 
 if __name__ == '__main__':
-    window = webview.create_window('Simple browser', 'https://vidsrc.to/embed/movie/tt8385148')
+    window = webview.create_window('Simple browser', 'https://vidsrc.to/embed/movie/tt8385148', confirm_close=True)
 
-    webview.start(toggle_fullscreen, window)
+    window.events.closed += on_closed
+    window.events.closing += on_closing
+    window.events.shown += on_shown
+    window.events.loaded += on_loaded
+    window.events.minimized += on_minimized
+    window.events.maximized += on_maximized
+    window.events.restored += on_restored
+    window.events.resized += on_resized
+    window.events.moved += on_moved
+
+    webview.start()
+
 
 
 
