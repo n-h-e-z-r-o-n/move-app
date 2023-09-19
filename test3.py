e367a1a4b813fb2ca30c720db98cf87cab606598
Hezron Na <hezron.w12@gmail.com>
2023-09-19 22:35:20 +0300

    Committed 2023 0Tu.223520

--- test3.py
+++ test3.py
@@ -1,25 +1,54 @@
-import tkinter as tk
-from webview2.core import WebView2
+from tkinter import Tk
+import clr
+from tkwebview2.tkwebview2 import WebView2, have_runtime, install_runtime
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Threading')
+from System.Windows.Forms import Control
+from System.Threading import Thread, ApartmentState, ThreadStart
 
-class App:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("400x300")
+# Event handler for when the web page finishes loading
+def on_navigation_completed(sender, args):
+    print("Navigation completed.")
+    # You can perform actions here after the page has loaded
 
-        self.webview2 = WebView2()
-        self.webview2.grid(row=0, column=0, sticky="NSEW")
+# Event handler for when a message is received from the web page
+def on_web_message_received(sender, args):
+    print("Received message from web page:", args.Message)
+    # You can process and respond to messages received from the web page here
 
-        # Register an event handler for the NavigationStarting event.
-        self.webview2.NavigationStarting += self.on_navigation_starting
+# Event handler for when the web view requests external navigation (e.g., clicking a link)
+def on_navigation_starting(sender, args):
+    print("Navigation starting to:", args.Uri)
+    # You can prevent navigation or perform actions before navigating here
 
-        self.root.mainloop()
+# Event handler for when the web page title changes
+def on_document_title_changed(sender, args):
+    print("Document title changed to:", args.Title)
+    # You can update your application's title or perform other actions here
 
-    def on_navigation_starting(self, sender, args):
-        # Get the URL that the WebView2 is navigating to.
-        url = args.Uri
+def main():
+    if not have_runtime():
+        install_runtime()
+    root = Tk()
+    root.title('tkwebview2 Event Capture')
+    root.geometry('1200x600+5+5')
 
-        # Print the URL to the console.
-        print("Navigating to:", url)
+    frame2 = WebView2(root, 500, 500)
+    frame2.pack(side='left', padx=20, fill='both', expand=True)
+
+    # Bind event handlers to WebView2 events
+    frame2.NavigationCompleted += on_navigation_completed
+    frame2.WebMessageReceived += on_web_message_received
+    frame2.NavigationStarting += on_navigation_starting
+    frame2.DocumentTitleChanged += on_document_title_changed
+
+    frame2.load_url('https://vidsrc.to/embed/movie/tt8385148')
+
+    root.mainloop()
 
 if __name__ == "__main__":
-    app = App()
+    t = Thread(ThreadStart(main))
+    t.ApartmentState = ApartmentState.STA
+    t.Start()
+    t.Join()
+
