83a48f2e74a8c24305b138dd90c36c66231320d6
Hezron Na <hezron.w12@gmail.com>
2023-09-19 23:06:33 +0300

    Committed 2023 0Tu.230633

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@ import webview
 if __name__ == '__main__':
     # Create a standard webview window
 
-    window = webview.create_window('Simple browser', 'https://pywebview.flowrl.com/hello')
+    window = webview.create_window('Simple browser', 'https://vidsrc.to/embed/movie/tt8385148')
     window.toggle_fullscreen()
     webview.start()
 
