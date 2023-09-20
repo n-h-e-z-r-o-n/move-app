9abc467e5432845921b6d6087790019a4bb9d003
Hezron Na <hezron.w12@gmail.com>
2023-09-20 20:20:34 +0300

    Committed 2023 0We.202034

--- test2.py
+++ test2.py
@@ -1,32 +1,16 @@
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
-import requests
-from io import BytesIO
+#Import the required libraries
+from tkinter import *
 
-def display_web_image():
-    url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the URL of your image
-    response = requests.get(url)
-    image_bytes = BytesIO(response.content)
-    image = Image.open(image_bytes)
+#Create an instance of Tkinter Frame
+win = Tk()
 
-    # Resize the image to match the frame's dimensions
-    image = image.resize((frame.winfo_width(), frame.winfo_height()), Image.LANCZOS)
+#Set the geometry
+win.geometry("700x350")
 
-    photo = ImageTk.PhotoImage(image)
-    label.config(image=photo)
-    label.image = photo  # Keep a reference to the image to prevent it from being garbage collected
+#Adding transparent background property
+win.wm_attributes('-transparentcolor', '#ab23ff')
 
-root = tk.Tk()
-root.title("Stretch Web Image to Fill Frame")
+#Create a Label
+Label(win, text= "Hello World!", font= ('Helvetica 18'), bg= '#ab23ff').pack(ipadx= 50, ipady=50, padx= 20)
 
-frame = ttk.Frame(root)
-frame.pack(fill="both", expand=True)
-
-label = ttk.Label(frame)
-label.pack(fill="both", expand=True)
-
-button = ttk.Button(root, text="Load Web Image", command=display_web_image)
-button.pack()
-
-root.mainloop()
+win.mainloop()
