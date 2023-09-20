cbff845aad72ec16aadcb50427b795b4da3b580a
Hezron Na <hezron.w12@gmail.com>
2023-09-20 21:45:57 +0300

    Committed 2023 0We.214556

--- test2.py
+++ test2.py
@@ -1,34 +1,31 @@
 import tkinter as tk
-from tkinter import PhotoImage
+from PIL import Image, ImageTk
 
-def create_gradient_image(width, height, color1, color2):
-    """
-    Create a gradient image with the specified width, height, and colors.
-    """
-    gradient_image = tk.PhotoImage(width=width, height=height)
-
-    for x in range(width):
-        r = int((color1[0] * (width - x) + color2[0] * x) / width)
-        g = int((color1[1] * (width - x) + color2[1] * x) / width)
-        b = int((color1[2] * (width - x) + color2[2] * x) / width)
-
-        pixel_color = f'#{r:02x}{g:02x}{b:02x}'
-        gradient_image.put(pixel_color, (x, 0))
-
-    return gradient_image
-
+# Create a tkinter window
 root = tk.Tk()
-root.title("Gradient Photo")
+root.geometry("400x400")
 
 # Create a gradient image
-gradient_color1 = (255, 0, 0)  # Red
-gradient_color2 = (0, 0, 255)  # Blue
-gradient_width = 400
-gradient_height = 300
-gradient_image = create_gradient_image(gradient_width, gradient_height, gradient_color1, gradient_color2)
+width, height = 400, 400
+gradient = Image.new("RGB", (width, height))
 
-# Create a label to display the gradient image
-gradient_label = tk.Label(root, image=gradient_image)
-gradient_label.place(relx=0.03, rely=0.04, relheight=0.4, relwidth=0.94)
+# Define the gradient colors (you can change these to create your desired gradient)
+color1 = (255, 0, 0)  # Red
+color2 = (0, 0, 255)  # Blue
+
+for y in range(height):
+    r = int(color1[0] + (color2[0] - color1[0]) * y / height)
+    g = int(color1[1] + (color2[1] - color1[1]) * y / height)
+    b = int(color1[2] + (color2[2] - color1[2]) * y / height)
+
+    for x in range(width):
+        gradient.putpixel((x, y), (r, g, b))
+
+# Convert the gradient image to a PhotoImage
+gradient_photo = ImageTk.PhotoImage(gradient)
+
+# Create a label to display the gradient photo
+label = tk.Label(root, image=gradient_photo)
+label.pack()
+
 root.mainloop()
-
