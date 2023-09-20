98129eb7501b1d5123e6678dcbe964aac56c84b8
Hezron Na <hezron.w12@gmail.com>
2023-09-21 00:39:17 +0300

    Committed 2023 0Th. 03917

--- test2.py
+++ test2.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 
-def on_entry_click(widget):
+def on_entry_click(widget, event):
     if widget.get() == "Placeholder Text":
         widget.delete(0, tk.END)
         widget.config(fg='black')  # Change text color to black
 
-def on_focusout(widget):
+def on_focusout(widget, event):
     if not widget.get():
         widget.insert(0, "Placeholder Text")
         widget.config(fg='gray')  # Change text color to gray
@@ -17,8 +17,8 @@ placeholder_text = "Placeholder Text"
 
 entry = tk.Entry(root, fg='gray')  # Set the initial text color to gray
 entry.insert(0, placeholder_text)
-entry.bind("<FocusIn>", lambda e:  on_entry_click(entry, e))
-entry.bind("<FocusOut>", lambda e : on_focusout(entry, e))
+entry.bind("<FocusIn>", lambda e:  on_entry_click(entry))
+entry.bind("<FocusOut>", lambda : on_focusout(entry))
 entry.pack()
 
 root.mainloop()
