80d3724bd72c58d3ad1ee29b92be25efaaef124f
Hezron Na <hezron.w12@gmail.com>
2023-09-20 22:48:33 +0300

    Committed 2023 0We.224833

--- test3.py
+++ test3.py
@@ -3,11 +3,11 @@ import tkinter as tk
 root = tk.Tk()
 
 # Create a Label widget with an image and text displayed above each other.
-label1 = tk.Label(root, compound=tk.TOP, text="This is some text.", image=tk.PhotoImage(file="image.gif"))
+label1 = tk.Label(root, compound=tk.TOP, text="This is some text.", image=tk.PhotoImage(file=r"C:\Users\HEZRON WEKESA\Pictures\12.png"))
 label1.pack()
 
 # Create a Button widget with an image and text displayed to the left of each other.
-button1 = tk.Button(root, compound=tk.LEFT, text="Click me!", image=tk.PhotoImage(file="image.gif"))
+button1 = tk.Button(root, compound=tk.BOTTOM, text="Click me!", image=tk.PhotoImage(file=r"C:\Users\HEZRON WEKESA\Pictures\12.png"))
 button1.pack()
 
 root.mainloop()
