43a404f504f12ac7b9c4c4457dab954ff403e768
Hezron Na <hezron.w12@gmail.com>
2023-09-20 18:03:28 +0300

    Committed 2023 0We.180328

--- test3.py
+++ test3.py
@@ -8,28 +8,32 @@ root = tk.Tk()
 root.title("Display Web Image in tkinter")
 root.state('zoomed')
 
-
 # Create a frame to hold the image
 image_frame = tk.Frame(root)
 image_frame.place(relx=0, rely=0, relheight=1, relwidth=1)
 
-# Define the URL of the web image
-image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
+def img(widget):
 
-# Download the image from the web
-response = requests.get(image_url)
-image_data = response.content
 
-# Create a PIL Image object from the image data
-image = Image.open(BytesIO(image_data))
+    # Define the URL of the web image
+    image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
 
-image_label = tk.Frame(image_frame, bg='blue')
+    # Download the image from the web
+    response = requests.get(image_url)
+    image_data = response.content
+
+    # Create a PIL Image object from the image data
+    image = Image.open(BytesIO(image_data))
+
+    image = image.resize((widget.winfo_width(), widget.winfo_height()), Image.LANCZOS)
+
+    # Create a PhotoImage object from the PIL Image
+    photo = ImageTk.PhotoImage(image)
+    return  photo
+
+
+image_label = tk.Frame(image_frame, bg='blue', )
 image_label.place(relx=0.03, rely=0.04, relheight=0.4, relwidth=0.94)
-
-image = image.resize((image_label.winfo_width(), image_label.winfo_height()), Image.LANCZOS)
-
-# Create a PhotoImage object from the PIL Image
-photo = ImageTk.PhotoImage(image)
 
 # Create a label to display the image
 image_label.config(image=photo)
