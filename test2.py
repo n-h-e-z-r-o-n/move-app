2ea5b06cecb77ed60d9da6ee4726d2112b394e1e
Hezron Na <hezron.w12@gmail.com>
2023-09-20 23:55:25 +0300

    Committed 2023 0We.235525

--- test2.py
+++ test2.py
@@ -1,12 +1,41 @@
-import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
+import tkinter as tk
+import time
+import colorsys
 
-root = ttk.Window(themename="superhero")
 
-b1 = ttk.Button(root, text="Submit", bootstyle="success")
-b1.pack(side=LEFT, padx=5, pady=10)
+def rgb_to_hex(rgb):
+    """Convert RGB color tuple to hexadecimal string."""
+    return "#{:02X}{:02X}{:02X}".format(*rgb)
 
-b2 = ttk.Button(root, text="Submit", bootstyle="info-outline")
-b2.pack(side=LEFT, padx=5, pady=10)
+
+def hsv_to_rgb(h, s, v):
+    """Convert HSV color to RGB color."""
+    return tuple(int(val * 255) for val in colorsys.hsv_to_rgb(h, s, v))
+
+
+def pulsing_color(label):
+    for i in range(360):  # Transition through hue values (0 to 359)
+        hue = i / 360.0
+        rgb_color = hsv_to_rgb(hue, 1, 1)  # Convert hue to RGB
+        hex_color = rgb_to_hex(rgb_color)
+
+        # Check if the color is not blue (#0000FF)
+        if hex_color != "#0000FF":
+            label.config(bg=hex_color)
+            label.update()  # Update the label's appearance
+
+        time.sleep(0.04)  # Adjust the delay as needed for the desired pulsing speed
+
+    root.after(1000, lambda: pulsing_color(label))
+
+
+root = tk.Tk()
+root.geometry("400x400")
+
+label = tk.Label(root, text="Pulsing Colors", width=20, height=5)
+label.pack()
+
+# Start the pulsing color loop
+pulsing_color(label)
 
 root.mainloop()
