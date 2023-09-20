df3bd3829cb105d4b00368363a550a82315fcb28
Hezron Na <hezron.w12@gmail.com>
2023-09-20 11:48:34 +0300

    Committed 2023 0We.114834

--- test.py
+++ test.py
@@ -92,7 +92,7 @@ def main():
         original_height = video_box.winfo_height()
 
         fullscreen_button = tk.Button(video_box, text="Expand to Full Screen", command=lambda: toggle_fullscreen(video_box))
-        fullscreen_button.place(relx=0.01, rely=0.01, relheight=0.1, relwidth=0.1)
+        fullscreen_button.place(relx=0.95, rely=0.95, relheight=0.05, relwidth=0.05)
 
         label2 = tk.Button(large_frame, bg='green',text="This is a label in the large frame", command=lambda:hold.full_screeen())
         label2.place(x = 0.1, rely=0.7, relheight = 0.1)
