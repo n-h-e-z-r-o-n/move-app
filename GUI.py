3acf7fda90618a961ccf2a4c09f56e45c32c9533
Hezron Na <hezron.w12@gmail.com>
2023-09-21 01:02:13 +0300

    Committed 2023 0Th. 10213

--- GUI.py
+++ GUI.py
@@ -63,9 +63,7 @@ def main():
             root.after(1000, lambda: change_color(widget))
 
         def pulsing_color(widget):
-            def con(hex_color):
-                widget.config(bg=hex_color)
-                widget.update()  # Update the label's appearance
+
             for i in range(360):  # Transition through hue values (0 to 359)
                 hue = i / 360.0
                 rgb_color = tuple(int(val * 255) for val in colorsys.hsv_to_rgb(hue, 1, 1))  # Convert hue to RGB
@@ -73,11 +71,11 @@ def main():
 
                 # Check if the color is not blue (#0000FF)
                 if hex_color != "#0000FF":
-                    #widget.config(bg=hex_color)
-                    #widget.update()  # Update the label's appearance
-                    root.after(3000, lambda: con(hex_color))
+                    widget.config(bg=hex_color)
+                    widget.update()  # Update the label's appearance
 
-                #time.sleep(0.04)  # Adjust the delay as needed for the desired pulsing speed
+
+                time.sleep(0.04)  # Adjust the delay as needed for the desired pulsing speed
 
             root.after(3000, lambda: pulsing_color(widget))
 
