3254744044af2c9c3055fba95d403ec5b4a7303b
Hezron Na <hezron.w12@gmail.com>
2023-09-21 00:07:54 +0300

    Committed 2023 0Th. 00754

--- test2.py
+++ test2.py
@@ -1,34 +1,24 @@
-import tkinter as tk
-import time
-import colorsys
+from tkinter import * 
 
+cnt=1
+def msgShow():
+    global cnt
+    # label["text"] = "I love Python x" + str(cnt)
+    # label["bg"] = "lightyellow"
+    # label["fg"] = "blue"
+    label.config(text="I love Python x" + str(cnt),
+            bg="lightyellow",fg="blue")
+    cnt += 1
 
+root = Tk()
+root.title("apidemos.com")
+root.geometry("300x300")
+label = Label(root)
+# label["text"] = "I love Java"
 
-
-
-def pulsing_color(widget):
-    for i in range(360):  # Transition through hue values (0 to 359)
-        hue = i / 360.0
-        rgb_color = tuple(int(val * 255) for val in colorsys.hsv_to_rgb(hue, 1, 1))   # Convert hue to RGB
-        hex_color = "#{:02X}{:02X}{:02X}".format(*rgb_color)
-
-        # Check if the color is not blue (#0000FF)
-        if hex_color != "#0000FF":
-            widget.config(bg=hex_color)
-            widget.update()  # Update the label's appearance
-
-        time.sleep(0.04)  # Adjust the delay as needed for the desired pulsing speed
-
-    root.after(1000, lambda: pulsing_color(widget))
-
-
-root = tk.Tk()
-root.geometry("400x400")
-
-label = tk.Label(root, text="Pulsing Colors", width=20, height=5)
+btn = Button(root,command=msgShow,
+                cursor="star")
 label.pack()
-
-# Start the pulsing color loop
-pulsing_color(label)
+btn.pack()
 
 root.mainloop()
