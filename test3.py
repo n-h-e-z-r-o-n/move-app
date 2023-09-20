d795c08112019159b934f442df9d80f7fe7ae4e8
Hezron Na <hezron.w12@gmail.com>
2023-09-20 18:09:44 +0300

    Committed 2023 0We.180944

--- test3.py
+++ test3.py
@@ -12,22 +12,26 @@ root.state('zoomed')
 # Create a frame to hold the image
 image_frame = tk.Frame(root)
 image_frame.place(relx=0, rely=0, relheight=1, relwidth=1)
+def imagen(widget):
+    # Define the URL of the web image
+    image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
 
-# Define the URL of the web image
-image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
+    # Download the image from the web
+    response = requests.get(image_url)
+    image_data = response.content
 
-# Download the image from the web
-response = requests.get(image_url)
-image_data = response.content
+    # Create a PIL Image object from the image data
+    image = Image.open(BytesIO(image_data))
 
-# Create a PIL Image object from the image data
-image = Image.open(BytesIO(image_data))
+    # Resize the image to match the frame's dimensions
+    image = image.resize((widget.winfo_width(), widget.winfo_height()), Image.LANCZOS)
 
-# Create a PhotoImage object from the PIL Image
-photo = ImageTk.PhotoImage(image)
+    # Create a PhotoImage object from the PIL Image
+    photo = ImageTk.PhotoImage(image)
+    return photo
 
 # Create a label to display the image
-image_label = tk.Label(image_frame, bg='blue', image=photo)
+image_label = tk.Label(image_frame, bg='blue', image=imagen(image_label))
 image_label.place(relx=0.03, rely=0.04, relheight=0.4, relwidth=0.94)
 
 # Keep a reference to the PhotoImage to prevent it from being garbage collected
