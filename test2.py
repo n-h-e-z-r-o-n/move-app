4679cfefc768dd6fcccb44eb5431cf5849c9bf3b
Hezron Na <hezron.w12@gmail.com>
2023-09-21 00:35:07 +0300

    Committed 2023 0Th. 03507

--- test2.py
+++ test2.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 
-class EntryWithPlaceholder(tk.Entry):
-    def __init__(self, master, placeholder, **kwargs):
-        super().__init__(master, **kwargs)
-        self.placeholder = placeholder
-        self.insert(0, placeholder)
+def on_entry_click(event):
+    if entry.get() == "Placeholder Text":
+        entry.delete(0, tk.END)
+        entry.config(fg='black')  # Change text color to black
 
-    def on_focus_in(self, event):
-        if self.get() == self.placeholder:
-            self.delete(0, "end")
-
-    def on_focus_out(self, event):
-        if self.get() == "":
-            self.insert(0, self.placeholder)
+def on_focusout(event):
+    if not entry.get():
+        entry.insert(0, "Placeholder Text")
+        entry.config(fg='gray')  # Change text color to gray
 
 root = tk.Tk()
+root.geometry("300x200")
 
-entry = EntryWithPlaceholder(root, placeholder="Enter your name")
+placeholder_text = "Placeholder Text"
+
+entry = tk.Entry(root, fg='gray')  # Set the initial text color to gray
+entry.insert(0, placeholder_text)
+entry.bind("<FocusIn>", lambda: on_entry_click(entry))
+entry.bind("<FocusOut>", on_focusout)
 entry.pack()
 
 root.mainloop()
