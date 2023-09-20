97c2b83c70c921463a2941d8a4a84226588e5992
Hezron Na <hezron.w12@gmail.com>
2023-09-20 22:46:28 +0300

    Committed 2023 0We.224628

--- test3.py
+++ test3.py
@@ -1,7 +1,13 @@
-import ttkbootstrap as ttk
+import tkinter as tk
 
-# traditional approach
-root = ttk.Tk()
-style = ttk.Style("darkly")
+root = tk.Tk()
 
-# new approach
+# Create a Label widget with an image and text displayed above each other.
+label1 = tk.Label(root, compound=tk.TOP, text="This is some text.", image=tk.PhotoImage(file="image.gif"))
+label1.pack()
+
+# Create a Button widget with an image and text displayed to the left of each other.
+button1 = tk.Button(root, compound=tk.LEFT, text="Click me!", image=tk.PhotoImage(file="image.gif"))
+button1.pack()
+
+root.mainloop()
