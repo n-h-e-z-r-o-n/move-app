aa7efa0965b933d510aab5b09b9c6d6fc29a7c6b
Hezron Na <hezron.w12@gmail.com>
2023-09-20 21:12:39 +0300

    Committed 2023 0We.211239

--- GUI.py
+++ GUI.py
@@ -109,7 +109,7 @@ def main():
         image_label.config(image=photo)
         play_bn = customtkinter.CTkButton(root, text="CTkButton")
         #play_bn = tk.Button(image_label, text='▶', font = ('Arial Black', 36), justify='center', bg = '#ab23ff', borderwidth=0, border=0, fg='white')
-        play_bn.place(relx=0.49, rely=0.47, relheight=0.04, relwidth=0.02)
+        play_bn.place(relx=0.49, rely=0.47, relheight=0.04, relwidth=0.2)
 
 
 
