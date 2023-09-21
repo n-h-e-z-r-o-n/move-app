e4076b89ab67bd1621c2cfaa7d191e981dc29dc3
Hezron Na <hezron.w12@gmail.com>
2023-09-21 09:39:11 +0300

    Committed 2023 0Th. 93911

--- test3.py
+++ test3.py
@@ -1,21 +1,30 @@
-from PIL import Image, ImageEnhance
+import numpy as np
+from PIL import Image
 
-# Load the image
-input_image_path = "input_image.jpg"
-output_image_path = "output_image.jpg"
-image =  Image.open(r"C:\Users\HEZRON WEKESA\Pictures\20220819_083721.jpg")
+# Load the image to be blended.
+im = Image.open(r"C:\Users\HEZRON WEKESA\Pictures\20220819_083721.jpg")
 
-# Define the number of steps for the fade effect
-num_steps = 10
+# Ensure the image has an alpha channel.
+im = im.convert("RGBA")
 
-# Create a list of images with gradually reduced brightness
-fade_images = []
-for step in range(num_steps + 1):
-    # Adjust brightness using ImageEnhance
-    brightness_factor = 1.0 - (step / num_steps)  # Decrease brightness
-    enhanced_image = ImageEnhance.Brightness(image).enhance(brightness_factor)
-    fade_images.append(enhanced_image)
+width, height = im.size
+pixels = im.load()
 
-# Save the faded images as a GIF or another format
-fade_images[0].save(output_image_path, save_all=True, append_images=fade_images[1:], duration=100, loop=0)
+# Define the top and bottom fade heights as a percentage of the image height.
+top_fade_height = int(height * 0.20)  # Adjust this value for the desired top fade height
+bottom_fade_height = int(height * 0.20)  # Adjust this value for the desired bottom fade height
 
+# Fade the top region to dark.
+for y in range(top_fade_height):
+    alpha = int((y / top_fade_height) * 255)
+    for x in range(width):
+        pixels[x, y] = pixels[x, y][:3] + (alpha,)
+
+# Fade the bottom region to dark.
+for y in range(height - bottom_fade_height, height):
+    alpha = int(((height - y) / bottom_fade_height) * 255)
+    for x in range(width):
+        pixels[x, y] = pixels[x, y][:3] + (alpha,)
+
+# Save the modified image.
+im.save('birdfade.png')
