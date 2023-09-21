530df349c59e1e4cffea1826054ec75d3b0d2510
Hezron Na <hezron.w12@gmail.com>
2023-09-21 10:23:15 +0300

    Committed 2023 0Th.102315

--- GUI.py
+++ GUI.py
@@ -120,7 +120,7 @@ def main():
                 widget.place(relx=0, rely=0, x=0, y=0, relwidth=1, relheight=screen_height/large_frame_size)
                 is_fullscreen = True
 
-        def imagen(widget):
+        def imlagen(widget):
             # Define the URL of the web image
             image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
 
@@ -136,6 +136,48 @@ def main():
 
             # Create a PhotoImage object from the PIL Image
             photo = ImageTk.PhotoImage(image)
+            return photo
+
+        def imagen(widget):
+            image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
+
+            # Download the image from the web
+            response = requests.get(image_url)
+            image_data = response.content
+
+            # Create a PIL Image object from the image data
+            image = Image.open(BytesIO(image_data))
+
+            # Resize the image to match the frame's dimensions
+            image = image.resize((1000, (1000)), Image.LANCZOS)
+
+            # Ensure the image has an alpha channel.
+            im = image.convert("RGBA")
+
+            width, height = im.size
+            pixels = im.load()
+
+            # Define the top and bottom fade heights as a percentage of the image height.
+            top_fade_height = int(height * 0.50)  # Adjust this value for the desired top fade height
+            bottom_fade_height = int(height * 0.50)  # Adjust this value for the desired bottom fade height
+
+            # Fade the top region to dark.
+            for y in range(top_fade_height):
+                alpha = int((y / top_fade_height) * 255)
+                for x in range(width):
+                    pixels[x, y] = pixels[x, y][:3] + (alpha,)
+
+            # Fade the bottom region to dark.
+            for y in range(height - bottom_fade_height, height):
+                alpha = int(((height - y) / bottom_fade_height) * 255)
+                for x in range(width):
+                    pixels[x, y] = pixels[x, y][:3] + (alpha,)
+
+            # Save the modified image.
+            im.save('birdfade.png')
+
+            # Create a PhotoImage object from the PIL Image
+            photo = ImageTk.PhotoImage(im)
             return photo
 
         def on_entry_click(widget, event):
