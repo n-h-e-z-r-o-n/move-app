0c4473b05f6432d78c9180c7aa36a35042d6ed3a
Hezron Na <hezron.w12@gmail.com>
2023-09-21 10:16:59 +0300

    Committed 2023 0Th.101658

--- test3.py
+++ test3.py
@@ -1,30 +1,26 @@
-import numpy as np
-from PIL import Image
+# Define the URL of the web image
+from io import BytesIO
 
-# Load the image to be blended.
-im = Image.open(r"C:\Users\HEZRON WEKESA\Pictures\20220819_083721.jpg")
+import requests
+from PIL import Image, ImageTk
 
-# Ensure the image has an alpha channel.
-im = im.convert("RGBA")
+def proc():
+        image_url = "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@.jpg"  # Replace with the actual image URL
 
-width, height = im.size
-pixels = im.load()
+        # Download the image from the web
+        response = requests.get(image_url)
+        image_data = response.content
 
-# Define the top and bottom fade heights as a percentage of the image height.
-top_fade_height = int(height * 0.20)  # Adjust this value for the desired top fade height
-bottom_fade_height = int(height * 0.20)  # Adjust this value for the desired bottom fade height
+        # Create a PIL Image object from the image data
+        image = Image.open(BytesIO(image_data))
 
-# Fade the top region to dark.
-for y in range(top_fade_height):
-    alpha = int((y / top_fade_height) * 255)
-    for x in range(width):
-        pixels[x, y] = pixels[x, y][:3] + (alpha,)
+        # Resize the image to match the frame's dimensions
+        image = image.resize((1000, (1000)), Image.LANCZOS)
 
-# Fade the bottom region to dark.
-for y in range(height - bottom_fade_height, height):
-    alpha = int(((height - y) / bottom_fade_height) * 255)
-    for x in range(width):
-        pixels[x, y] = pixels[x, y][:3] + (alpha,)
+        
 
-# Save the modified image.
-im.save('birdfade.png')
+
+
+
+
+proc()
