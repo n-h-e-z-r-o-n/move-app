c8d6f5e48cf5c3ef26bb3fe06a8f08107c0f6126
Hezron Na <hezron.w12@gmail.com>
2023-09-22 21:53:58 +0300

    Committed 2023 0Fr.215358

--- test3.py
+++ test3.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+import tkinter as tk
 import clr
 from tkwebview2.tkwebview2 import WebView2, have_runtime, install_runtime
 clr.AddReference('System.Windows.Forms')
@@ -11,13 +11,14 @@ def on_double_click():
 def main():
     if not have_runtime():#没有webview2 runtime
         install_runtime()
-    root=Tk()
+    root=tk.Tk()
     root.title('pywebview for tkinter test')
     root.geometry('1200x600+5+5')
 
 
-
-    frame2=WebView2(root,500,500)
+    p = tk.Frame(root)
+    p.place(relheight=1,relwidth=1,rely=0,relx=0)
+    frame2=WebView2(p,500,500)
     frame2.pack(side='left',padx=0,fill='both',expand=True)
     frame2.load_url('https://vidsrc.to/embed/movie/tt4154796')
 
