d9a28135eef2ffb76aefeab98648ccdf34ada7ae
Hezron Na <hezron.w12@gmail.com>
2023-09-22 16:29:11 +0300

    Committed 2023 0Fr.162911

--- GUI.py
+++ GUI.py
@@ -360,12 +360,12 @@ def main():
                 image_label.image = img
 
 
-            label2 = tk.Button(large_frame, bg='green')
+            label2 = tk.Button(large_frame, bg='green', bd=0, border=0)
             label2.place(relx=0.04, rely=0.78, relheight=0.2, relwidth=0.15)
 
             p = tk.Button(label2)
             p.place(relx=0,rely=0, relwidth=1, relheight=1)
-            p_i = imagen(movie_poster_url, 250, 317)
+            p_i = imagen(movie_poster_url, 280, 396)
             p.config(image=p_i)
             p.image = p_i
 
