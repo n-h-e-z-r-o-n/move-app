93ae8487c1b1ccb7d522dbb0c6a41c9464f2323a
Hezron Na <hezron.w12@gmail.com>
2023-09-22 16:01:13 +0300

    Committed 2023 0Fr.160113

--- test3.py
+++ test3.py
@@ -1,36 +1,5 @@
-import nltk
-import imdb
+from PyMovieDb import IMDB
+imdb = IMDB()
+res = imdb.popular_movies(genre=None, start_id=1, sort_by=None)
 
-ia = imdb.Cinemagoer()
-# Set up SQLite caching
-movies = ia.search_movie('Avengers: Endgame')
-# Fetch additional details, including images
-ia.update(movies[0])
-def plotp():
-    plot_str = ''
-    for i in movies[0]["plot"]:
-        plot_str += str(i)
-    return plot_str
-
-# Sample text
-text = plotp()
-
-print(text)
-
-# Download the NLTK sentence tokenizer data if not already downloaded
-nltk.download("punkt")
-
-# Import the NLTK sentence tokenizer
-from nltk.tokenize import sent_tokenize
-
-
-
-# Tokenize the text into sentences
-sentences = sent_tokenize(text)
-
-# Get the first sentence
-if sentences:
-    first_sentence = sentences[0]
-    print("First Sentence:", first_sentence)
-else:
-    print("No sentences found in the text.")
+print(res)
