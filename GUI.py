41cf6002a8ad7368d11b11996ca7b200fd35bb46
Hezron Na <hezron.w12@gmail.com>
2023-09-22 15:21:05 +0300

    Committed 2023 0Fr.152105

--- GUI.py
+++ GUI.py
@@ -312,7 +312,7 @@ def main():
             Type16.place(relx=0.26, rely=0.658, relheight=0.026, relwidth=0.68)
 
             Type6 = tk.Label(large_frame, bg="green", fg='gray', text="Plot : ", justify=tk.LEFT, anchor=tk.W, font=('Comic Sans MS', 12, "bold"))
-            Type6.place(relx=0.19, rely=0.689, relheight=0.016, relwidth=0.06)
+            Type6.place(relx=0.04, rely=0.689, relheight=0.016, relwidth=0.06)
 
             Type16 = tk.Text(large_frame, bg='green', fg='white', borderwidth=0, border=0, wrap=tk.WORD, font=('Comic Sans MS', 12))
             Type16.insert("end", movie_plot)
