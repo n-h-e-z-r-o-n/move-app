ac7b82e901d2fcf2e91058dcb0a7588fa90b87e3
Hezron Na <hezron.w12@gmail.com>
2023-09-22 20:27:08 +0300

    Committed 2023 0Fr.202708

--- test3.py
+++ test3.py
@@ -1,5 +1,30 @@
-from PyMovieDb import IMDB
-imdb = IMDB()
-res = imdb.popular_movies(genre=None, start_id=1, sort_by=None)
+from tkinter import Tk
+from tkwebview2.tkwebview2 import WebView2, have_runtime, install_runtime
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Threading')
+from System.Windows.Forms import Control
+from System.Threading import Thread,ApartmentState,ThreadStart
 
-print(res)
+#范例
+def main():
+    if not have_runtime():#没有webview2 runtime
+        install_runtime()
+    root=Tk()
+    root.title('pywebview for tkinter test')
+    root.geometry('1200x600+5+5')
+
+    frame=WebView2(root,500,500)
+    frame.pack(side='left')
+    frame.load_html('<h1>hi hi</h1>')
+
+    frame2=WebView2(root,500,500)
+    frame2.pack(side='left',padx=20,fill='both',expand=True)
+    frame2.load_url('https://smart-space.com.cn/')
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    t = Thread(ThreadStart(main))
+    t.ApartmentState = ApartmentState.STA
+    t.Start()
+    t.Join()
