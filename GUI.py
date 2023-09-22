d14a96fc8dccc4e9af3ab52b12141691775c0c28
Hezron Na <hezron.w12@gmail.com>
2023-09-22 21:04:10 +0300

    Committed 2023 0Fr.210410

--- GUI.py
+++ GUI.py
@@ -434,7 +434,10 @@ def main():
             original_width = widget.winfo_width()
             original_height = widget.winfo_height()
 
-            
+            fullscreen_button = tk.Button(widget, border=0, borderwidth=0, text="⤢", bg='black', justify='center', activebackground='black', activeforeground='white', fg='white', font=('Arial Black', 26), command=lambda: toggle_fullscreen(video_box, original_x, original_y, original_width, original_height))
+            fullscreen_button.place(relx=0.97, rely=0.95, relheight=0.05, relwidth=0.03)
+
+
 
 
 
@@ -449,8 +452,7 @@ def main():
 
 
 
-        fullscreen_button = tk.Button(video_box, border=0, borderwidth=0, text="⤢", bg='black', justify='center', activebackground='black', activeforeground='white', fg='white', font=('Arial Black', 26), command=lambda: toggle_fullscreen(video_box, original_x, original_y, original_width, original_height))
-        fullscreen_button.place(relx=0.97, rely=0.95, relheight=0.05, relwidth=0.03)
+
 
     watch_page(frame, movie_id, movie_title, movie_ratting, movie_type, movie_country, movie_genres, movie_year, movie_production_company, movie_cast_names, movie_plot, movie_poster_url, r1_title, r1_year, recomednation_1_poster, r2_title, r2_year, recomednation_2_poster, r3_title, r3_year, recomednation_3_poster, r4_title, r4_year, recomednation_4_poster, r5_title, r5_year, recomednation_5_poster, r6_title, r6_year, recomednation_6_poster)
 
