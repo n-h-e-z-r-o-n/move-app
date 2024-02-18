2387b6de7e6809fa5c7afff7ed801aa08957cbce
Hezron Na <hezron.w12@gmail.com>
2024-02-18 20:46:49 +0300

    Committed 2024 0Su.204649

--- GUI.py
+++ GUI.py
@@ -1093,9 +1093,9 @@ def main():
     Backround = tk.Canvas(root, bg='yellow', width=screen_width, height=screen_height)
     Backround.place(relx=0, rely=0, relwidth=1, relheight=1)
     bg_photo = Image.open("./Assets/12.jpg")
-    resized_background_image = bg_photo.resize((1000, 1000), Image.LANCZOS)
+    resized_background_image = bg_photo.resize((screen_width, screen_height), Image.LANCZOS)
     bg_photo1 = ImageTk.PhotoImage(resized_background_image)
-    Backround.create_image(0 , 0, image=bg_photo1, anchor="center")
+    Backround.create_image(0 , 0, image=bg_photo1, anchor="nw")
     #imagen("./Assets/12.jpg",screen_width , Home_frame_hight, Backround)
 
     """
