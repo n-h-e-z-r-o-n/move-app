2e1954888e04445fad610717bd379ab32a53e99f
Hezron Na <hezron.w12@gmail.com>
2024-02-19 00:21:04 +0300

    Committed 2024 0Mo. 02104

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import movieposters as mp
 link = mp.get_poster(title='breakfast club')
-assert link == mp.get_poster(id='tt385687')  # can also be found using movie's id
-assert link == mp.get_poster(id=88847)
+assert link == mp.get_poster(id='tt0088847')  # can also be found using movie's id
+assert link == mp.get_poster(id=385687)
 assert link == 'https://m.media-amazon.com/images/M/MV5BOTM5N2ZmZTMtNjlmOS00YzlkLTk3YjEtNTU1ZmQ5OTdhODZhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX380_CR0,16,380,562_.jpg'
