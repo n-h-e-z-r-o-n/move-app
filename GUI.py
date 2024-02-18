c48278763d0a94d7149777b1248508e27827c650
Hezron Na <hezron.w12@gmail.com>
2024-02-18 20:37:41 +0300

    Committed 2024 0Su.203740

--- GUI.py
+++ GUI.py
@@ -1090,12 +1090,12 @@ def main():
     search_q = tk.StringVar()
     Home_frame_hight = screen_height * 5
 
-    Backround = tk.Canvas(root)
+    Backround = tk.Canvas(root, bg='yellow')
     Backround.place(relx=0, rely=0, relwidth=1, relheight=1)
     bg_photo = Image.open("./Assets/12.jpg")
     resized_background_image = bg_photo.resize((screen_width, screen_height), Image.LANCZOS)
     bg_photo1 = ImageTk.PhotoImage(resized_background_image)
-    Backround.create_image(screen_height,screen_height, image=bg_photo1)
+    Backround.create_image(0 , 0, image=bg_photo1)
     #imagen("./Assets/12.jpg",screen_width , Home_frame_hight, Backround)
 
     """
