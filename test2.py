44a20673b98412e888cebbc92e650643858cc8ba
Hezron Na <hezron.w12@gmail.com>
2024-02-18 23:27:42 +0300

    Committed 2024 0Su.232742

--- test2.py
+++ test2.py
@@ -13,8 +13,12 @@ print(start_run_time_time-end)
 def get_new_movies (page = 1):
     r = requests.get('https://vidsrc.to/vapi/movie/new/1')  # latest movies
     print(r.status_code)
+    movies = None
+    length = 0
     if r.status_code == 200:
         data = r.json()
         print(len(data['result']['items']))
         print(data['result']['items'])
-    end = time.time()
+
+    return movies , length
+
