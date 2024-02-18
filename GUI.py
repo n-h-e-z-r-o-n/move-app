0342a7fda520cc2655bb7d2ce6596de715c98d67
Hezron Na <hezron.w12@gmail.com>
2024-02-18 21:34:36 +0300

    Committed 2024 0Su.213436

--- GUI.py
+++ GUI.py
@@ -1089,14 +1089,14 @@ def main():
     search_q = tk.StringVar()
     Home_frame_hight = screen_height * 5
 
-    Backround = tk.Canvas(root, bg='yellow', width=screen_width, height=screen_height, borderwidth=0, highlightthickness=0)
+    Backround = tk.Label(root, bg='yellow', width=screen_width, height=screen_height, borderwidth=0, highlightthickness=0)
     Backround.place(relx=0, rely=0, relwidth=1, relheight=1)
-    """
-    bg_photo = Image.open("./Assets/12.jpg")
-    resized_background_image = bg_photo.resize((screen_width, screen_height), Image.LANCZOS)
-    bg_photo1 = ImageTk.PhotoImage(resized_background_image)
-    Backround.create_image(0 , 0, image=bg_photo1, anchor="nw")
-    #imagen("./Assets/12.jpg",screen_width , Home_frame_hight, Backround)
+    #bg_photo = Image.open("./Assets/12.jpg")
+    #resized_background_image = bg_photo.resize((screen_width, screen_height), Image.LANCZOS)
+    #bg_photo1 = ImageTk.PhotoImage(resized_background_image)
+
+
+    imagen("./Assets/12.jpg",screen_width , Home_frame_hight, Backround)
     """
 
     FRAME_1 = tk.Frame(root)
@@ -1114,7 +1114,7 @@ def main():
 
     widget_track_position.append(Home_frame)
     page_count += 1
-
+    """
     """
     FRAME_2 = tk.Frame(root, bg='black')
     FRAME_2.place(relwidth=1, relheight=1, relx=0, rely=0)
