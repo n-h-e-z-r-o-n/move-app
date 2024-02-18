609b190781d7d1bd84831325f2228d3fb4eb9a4e
Hezron Na <hezron.w12@gmail.com>
2024-02-18 18:58:16 +0300

    Committed 2024 0Su.185816

--- GUI.py
+++ GUI.py
@@ -13,9 +13,10 @@ import threading
 from imdbmovies import IMDB
 import imdb
 import requests
+import ctypes as ct # used for dark mode
 
+# =================================================  integrating pywebview  ===========================================================================
 # =====================================================================================================================================================
-# integrating pywebview
 import ctypes
 from webview.window import Window
 from webview.platforms.edgechromium import EdgeChrome
@@ -87,6 +88,17 @@ imdb_other = IMDB()
 
 # ================= Functions Definition ==============================================================================================================
 # =====================================================================================================================================================
+
+def dark_title_bar(window):  # dark mode navegation bar
+    window.update()
+    DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ct.windll.user32.GetParent
+    hwnd = get_parent(window.winfo_id())
+    rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+    value = 2
+    value = ct.c_int(value)
+    set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
 
 def clean_url(url):
     url = url
