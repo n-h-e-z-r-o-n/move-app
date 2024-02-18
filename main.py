a4cbe0a042d4d2bbcb5673b0bf97e7de4e99529a
Hezron Na <hezron.w12@gmail.com>
2024-02-19 00:39:49 +0300

    Committed 2024 0Mo. 03949

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import imdb
 ia = imdb.Cinemagoer()
-movies = ia.get_movie(25406412)
+m='tt22488984'
+movies = ia.get_movie(m[2:])
 movie_poster_url = movies.get('full-size cover url')
 
 print(movie_poster_url)
+
+def 
