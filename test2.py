5c33775869608abb1ac8307aa5e0da8341c40069
Hezron Na <hezron.w12@gmail.com>
2024-02-19 22:00:34 +0300

    Committed 2024 0Mo.220034

--- test2.py
+++ test2.py
@@ -1,18 +1,18 @@
 from imdbmovies import IMDB
 
-#def Selected(movie_id):
-imdb_other = IMDB()
-movies = imdb_other.get_by_id("tt0944947")
-movie_name = movies['name']
-movie_type = movies['type']
-movie_genre = " ".join(i for i in movies['genre'])
-movie_datePublished = movies['datePublished']
-movie_ratingValue = movies['rating']['ratingValue']
-movie_poster_url = movies['poster']
-movie_actor = ", ".join(i['name'] for i in movies['actor'])
-movie_description = movies['description']
+def Selected(movie_id):
+    imdb_other = IMDB()
+    movies = imdb_other.get_by_id("tt0944947")
+    movie_name = movies['name']
+    movie_type = movies['type']
+    movie_genre = ", ".join(i for i in movies['genre'])
+    movie_datePublished = movies['datePublished']
+    movie_ratingValue = movies['rating']['ratingValue']
+    movie_poster_url = movies['poster']
+    movie_actor = ", ".join(i['name'] for i in movies['actor'])
+    movie_description = movies['description']
 
-#return movie_name, movie_type, movie_genre, movie_datePublished, movie_ratingValue, movie_poster_url, movie_actor, movie_description
+    return movie_name, movie_type, movie_genre, movie_datePublished, movie_ratingValue, movie_poster_url, movie_actor, movie_description
 
 
 print(movie_actor)
