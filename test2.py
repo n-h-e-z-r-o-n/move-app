77f90cb8275d3d1206e9fb9fa9350bddaa9529c7
Hezron Na <hezron.w12@gmail.com>
2024-02-19 21:56:23 +0300

    Committed 2024 0Mo.215623

--- test2.py
+++ test2.py
@@ -9,12 +9,12 @@ movie_genre = movies['genre']
 movie_datePublished = movies['datePublished']
 movie_ratingValue = movies['rating']['ratingValue']
 movie_poster_url = movies['poster']
-#movie_actor = "".join(i for i in movies['actor']['name'])
+movie_actor = "".join(i['name'] for i in movies['actor'])
 movie_description = movies['description']
 
 #return movie_name, movie_type, movie_genre, movie_datePublished, movie_ratingValue, movie_poster_url, movie_actor, movie_description
 
-for i in movies['actor']['name']
+for i in movies['actor']:
     print(i)
-#print(movie_actor)
+print(movie_actor)
 
