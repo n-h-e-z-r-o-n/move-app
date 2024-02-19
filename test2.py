07c35a6a8d61454029b131b3268b1b58b2ded976
Hezron Na <hezron.w12@gmail.com>
2024-02-19 21:52:11 +0300

    Committed 2024 0Mo.215211

--- test2.py
+++ test2.py
@@ -1,19 +1,18 @@
 from imdbmovies import IMDB
 
-def Selected(movie)
+#def Selected(movie_id):
 imdb_other = IMDB()
 movies = imdb_other.get_by_id("tt0944947")
-print(movies)
 movie_name = movies['name']
 movie_type = movies['type']
 movie_genre = movies['genre']
 movie_datePublished = movies['datePublished']
 movie_ratingValue = movies['rating']['ratingValue']
 movie_poster_url = movies['poster']
-movie_actor = movies['actor']
+movie_actor = "".join(i for i in movies['actor']['name'])
 movie_description = movies['description']
 
+#return movie_name, movie_type, movie_genre, movie_datePublished, movie_ratingValue, movie_poster_url, movie_actor, movie_description
 
 print(movie_actor)
-
 
