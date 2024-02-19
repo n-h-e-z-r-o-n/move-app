c076b562686505dc77c97e8f5215c5624e834334
Hezron Na <hezron.w12@gmail.com>
2024-02-19 21:28:49 +0300

    Committed 2024 0Mo.212849

--- test2.py
+++ test2.py
@@ -1,9 +1,8 @@
 from imdbmovies import IMDB
 imdb_other = IMDB()
 movies = imdb_other.get_by_id("tt0944947")
-movie_type = movies['']
+movie_type = movies['type']
 movie_name = movies['name']
 
 movie_poster_url = movies['poster']
-movie_name = movies['title']
-movie_name = movies['title']
+
