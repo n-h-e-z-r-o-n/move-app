040bb91a265a58943dc17209f13688a46db910c8
Hezron Na <hezron.w12@gmail.com>
2024-02-19 18:37:28 +0300

    Committed 2024 0Mo.183728

--- GUI.py
+++ GUI.py
@@ -129,24 +129,21 @@ def change_fg_OnHover(widget, colorOnHover, colorOnLeave):  # Color change fg on
     widget.bind("<Leave>", func=lambda e: widget.config(fg=colorOnLeave))
 
 
-def smooth_scroll(widget, increment):
-    # Get the current scroll position
-    current_scroll = float(widget.yview()[0])
 
-    # Calculate the new scroll position
-    new_scroll = max(0.0, min(1.0, current_scroll + increment))
-    print("new_scroll",new_scroll)
-    # Set the new scroll position
-    widget.yview_moveto(new_scroll)
 
 def on_mouse_wheel(widget, event):  # Function to handle mouse wheel scrolling
     # Scroll the canvas up or down based on the mouse wheel direction
     if event.delta < 0:
         #widget.yview_scroll(1, "units")
-        smooth_scroll(widget, 0.01)
+        current_scroll = float(widget.yview()[0])
+        new_scroll = max(0.0, min(1.0, current_scroll + 0.01))
+        widget.yview_moveto(new_scroll)
+
     else:
         #widget.yview_scroll(-1, "units")
-        smooth_scroll(widget, -0.01)
+        current_scroll = float(widget.yview()[0])
+        new_scroll = max(0.0, min(1.0, current_scroll + (-0.01)))
+        widget.yview_moveto(new_scroll)
 
 
 def widget_scroll_bind(widget):
