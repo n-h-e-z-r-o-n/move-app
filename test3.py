4be24fac89e2c9265b43e7c766e1623170101571
Hezron Na <hezron.w12@gmail.com>
2024-02-20 22:14:06 +0300

    Committed 2024 0Tu.221406

--- test3.py
+++ test3.py
@@ -1,15 +1,3 @@
-import requests
-def get_new_tv_shows (page = 1):
-    r = requests.get(f'https://vidsrc.to/vapi/tv/new/{page}')  # latest movies
-    print(r.status_code)
-    movies = None
-    length = 0
-    if r.status_code == 200:
-        data = r.json()
-        length = len(data['result']['items'])
-        movies = data['result']['items']
-    return movies, length
-
-movies, length = get_new_tv_shows()
-
-print(movies[0]['name'])
+import random
+m = random.randint(0, 1)
+print("random",  m)
