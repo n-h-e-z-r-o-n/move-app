503481189ae65b4dc2d76e2ec65e526287a885d2
Hezron Na <hezron.w12@gmail.com>
2024-02-21 17:14:40 +0300

    Committed 2024 0We.171440

--- GUI.py
+++ GUI.py
@@ -889,13 +889,16 @@ def Fetch_Mount(numer=24):
     global New_moves, Added_moves, New_TV_Shows, Added_TV_Shows
 
     def xr(shows_list):
+        M_list = []
         for movie in shows_list:
             movie_id = movie['imdb_id']
             title = movie['title']
             year = ''
             poster =  poster_image_get(movie_id)
 
-            movie_list.append((title, year, poster, movie_id))  # (title, year, post_url, movie_id)
+            M_list.append((title, year, poster, movie_id))  # (title, year, post_url, movie_id)
+
+        return M_list
 
     new_movie_list = []
     added_movie_list = []
@@ -915,10 +918,7 @@ def Fetch_Mount(numer=24):
 
         count += 1
 
-
-
-
-    return movie_list
+    xr(shows_list)
 
 
 # ---------------------------------------------------------------------------------------------------------------
