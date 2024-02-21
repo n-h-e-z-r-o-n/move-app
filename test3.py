ff341f08a666c2cfa93ffe911186d432ee709e1d
Hezron Na <hezron.w12@gmail.com>
2024-02-21 16:59:57 +0300

    Committed 2024 0We.165957

--- test3.py
+++ test3.py
@@ -1,20 +1,14 @@
 # (title, year, post_url, movie_id)
 import requests
-def get_added_movies(page=1):
-    r = requests.get(f'https://vidsrc.to/vapi/movie/add/{1}')  # latest movies
-    movies = None
-    length = 0
-    if r.status_code == 200:
-        data = r.json()
-        length = len(data['result']['items'])
-        movies = data['result']['items']
+def (page=1):
+
     movie_list = []
     for movie in movies:
         movie_id = movie['imdb_id']
         title = movie['title']
         year = ''
         poster = '' #poster_image_get(movie_id)
-        movie_list.append((title, year, poster, movie_id)) # (title, year, post_url, movie_id)
+        movie_list.append((title, year, poster, movie_id))  # (title, year, post_url, movie_id)
 
     return movie_list
 
