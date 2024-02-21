6d008f6ec795ed194812644b26808f610ae24e9d
Hezron Na <hezron.w12@gmail.com>
2024-02-21 10:28:45 +0300

    Committed 2024 0We.102845

--- GUI.py
+++ GUI.py
@@ -137,13 +137,13 @@ def on_mouse_wheel(widget, event):  # Function to handle mouse wheel scrolling
 
     # Scroll the canvas up or down based on the mouse wheel direction
     if event.delta < 0:
-        #widget.yview_scroll(1, "units")
-        xxx(widget, 0.001)
+        widget.yview_scroll(1, "units")
+        #xxx(widget, 0.004)
         #widget.update_idletasks()  # Force update of the display
 
     else:
-        #widget.yview_scroll(-1, "units")
-        xxx(widget, -0.001)
+        widget.yview_scroll(-1, "units")
+        #xxx(widget, -0.004)
         #widget.update_idletasks()  # Force update of the display
 
 
