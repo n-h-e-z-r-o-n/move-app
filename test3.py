f71c5e21a0beaa6b2e41099975cb6b02d13f255b
Hezron Na <hezron.w12@gmail.com>
2024-02-21 23:22:08 +0300

    Committed 2024 0We.232208

--- test3.py
+++ test3.py
@@ -1,3 +1,19 @@
-# (title, year, post_url, movie_id)
-t = 'httpskibttbt'
-print(t.startswith("htt"))
+import socket
+
+def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
+    """
+    Check internet connection by attempting to create a socket connection
+    to a reliable external server (default is Google's public DNS server).
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error:
+        return False
+
+# Check internet connection
+if check_internet_connection():
+    print("Internet connection available")
+else:
+    print("No internet connection")
