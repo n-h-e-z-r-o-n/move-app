fed09776859b7de0bdad22cb1a9b548a37513190
Hezron Na <hezron.w12@gmail.com>
2024-02-22 15:46:59 +0300

    Committed 2024 0Th.154659

--- GUI.py
+++ GUI.py
@@ -762,17 +762,20 @@ threading.Thread(target=has_internet_connection).start()
 # ------------------------------- Movie fetch function -------- -----------------------------------------------
 def get_new_movies(page=1):
     global internet_check,  closed
+    movies =[]
+    length = 0
     if not closed:
         while True:
-            if internet_check
-            r = requests.get(f'https://vidsrc.to/vapi/movie/new/{page}')  # latest movies
-            print(r.status_code)
-            movies = None
-            length = 0
-            if r.status_code == 200:
-                data = r.json()
-                length = len(data['result']['items'])
-                movies = data['result']['items']
+            if internet_check:
+                r = requests.get(f'https://vidsrc.to/vapi/movie/new/{page}')  # latest movies
+                print(r.status_code)
+                movies = None
+                length = 0
+                if r.status_code == 200:
+                    data = r.json()
+                    length = len(data['result']['items'])
+                    movies = data['result']['items']
+                    break
     return movies, length
 
 
