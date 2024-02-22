116b6cd419d1b6f78e58a6193fd7bc9aaf89e4a2
Hezron Na <hezron.w12@gmail.com>
2024-02-22 15:02:55 +0300

    Committed 2024 0Th.150255

--- GUI.py
+++ GUI.py
@@ -1177,8 +1177,12 @@ def Home_Page(widget):
 
     imagen_2("./Assets/footer.jpg", int(screen_width * 1), int(Home_frame_hight * 0.166), Home_label)
 
-    Home_label = tk.Label(Home_label, bg=bg_sections)
+    Home_label = tk.Frame(Home_label, bg=bg_sections)
     Home_label.place(relx=0, rely=0.9, relwidth=1, relheight=0.05)
+
+    Home_label = tk.Frame(Home_label, bg=bg_sections)
+    Home_label.place(relx=0, rely=0.9, relwidth=1, relheight=0.05)
+
 
     # ================================= Home threads  =====================================================================================================================================
 
