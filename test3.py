5925f3f2d755444a0c9ce857ee842c841fd5d3e2
Hezron Na <hezron.w12@gmail.com>
2024-02-22 14:46:35 +0300

    Committed 2024 0Th.144635

--- test3.py
+++ test3.py
@@ -1,8 +1,6 @@
 import socket
 
-def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
-
-    """
+def check_internet_connection(host="8.8.8.8", port=1, timeout=3):
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
