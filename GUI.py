f6602320cd2202645c4effc7190c8ccd41a3a20a
Hezron Na <hezron.w12@gmail.com>
2024-02-23 12:28:46 +0300

    Committed 2024 0Fr.122846

--- GUI.py
+++ GUI.py
@@ -459,7 +459,6 @@ def watch_page(widget, movie_id, movie_title, movie_ratting, movie_type, movie_c
     Add = tk.Frame(large_frame, bg='green',  borderwidth=0, border=0)
     Add.place(relx=0.04, rely=0.727, relheight=0.015, relwidth=0.95)
 
-
     tk.Button(Add, text="Season ", borderwidth=0, border=0, font=('Comic Sans MS', 12)).place(relheight=1 ,relwidth=0.1, relx=0, rely=0)
 
     Add2 = tk.Frame(large_frame, bg='green', borderwidth=0, border=0)
