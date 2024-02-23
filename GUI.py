e06d12044027500acc4b9b3887520b8a94326917
Hezron Na <hezron.w12@gmail.com>
2024-02-23 13:06:32 +0300

    Committed 2024 0Fr.130632

--- GUI.py
+++ GUI.py
@@ -455,26 +455,6 @@ def watch_page(widget, movie_id, movie_title, movie_ratting, movie_type, movie_c
     plot_wdget.config(state="disabled")
     plot_wdget.place(relx=0.04, rely=0.689, relheight=0.035, relwidth=0.95)
 
-    # ===================================================================================================================================
-    Add = tk.Frame(large_frame, bg='green',  borderwidth=0, border=0)
-    Add.place(relx=0.04, rely=0.727, relheight=0.015, relwidth=0.95)
-
-    Add2 = tk.Frame(large_frame, bg='green', borderwidth=0, border=0)
-    #Add2.place(relx=0.04, rely=0.742, relheight=0.22, relwidth=0.2)
-
-    global displayed
-    displayed = 0
-    def show_season(Add2):
-        global displayed
-        if displayed == 0:
-            Add2.place(relx=0.04, rely=0.742, relheight=0.22, relwidth=0.2)
-            displayed = 1
-        else:
-            Add2.place_forget()
-            displayed = 0
-
-
-    tk.Button(Add, text="Season ", borderwidth=0, border=0, font=('Comic Sans MS', 12), command= lambda widg=Add2:show_season(widg)).place(relheight=1, relwidth=0.1, relx=0, rely=0)
 
 
 
@@ -584,6 +564,28 @@ def watch_page(widget, movie_id, movie_title, movie_ratting, movie_type, movie_c
     frame2.load_url(f'https://vidsrc.to/embed/tv/{movie_id}')  # https://vidsrc.to/embed/movie/tt{movie_id}
     frame2.place(relheight=1, relwidth=1, relx=0, rely=0)
 
+    # ===================================================================================================================================
+    Add = tk.Frame(large_frame, bg='green', borderwidth=0, border=0)
+    Add.place(relx=0.04, rely=0.727, relheight=0.015, relwidth=0.95)
+
+    Add2 = tk.Frame(large_frame, bg='green', borderwidth=0, border=0)
+    # Add2.place(relx=0.04, rely=0.742, relheight=0.22, relwidth=0.2)
+
+    global displayed
+    displayed = 0
+
+    def show_season(Add2):
+        global displayed
+        if displayed == None: displayed = 0
+
+        if displayed == 0:
+            Add2.place(relx=0.04, rely=0.742, relheight=0.22, relwidth=0.2)
+            displayed = 1
+        else:
+            Add2.place_forget()
+            displayed = 0
+
+    tk.Button(Add, text="Season ", borderwidth=0, border=0, font=('Comic Sans MS', 12), command=lambda widg=Add2: show_season(widg)).place(relheight=1, relwidth=0.1, relx=0, rely=0)
 
     if random.randint(0, 1):
         threading.Thread(target=recommendation_movies, args=(recomedation_other, i_widh, i_high)).start()
