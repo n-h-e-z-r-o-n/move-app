0f6903a8431928931cdde8fa5816c4c2144ce9f5
Hezron Na <hezron.w12@gmail.com>
2024-02-23 13:12:48 +0300

    Committed 2024 0Fr.131248

--- GUI.py
+++ GUI.py
@@ -570,7 +570,7 @@ def watch_page(widget, movie_id, movie_title, movie_ratting, movie_type, movie_c
 
     Add2 = tk.Frame(large_frame, bg='green', borderwidth=0, border=0)
     # Add2.place(relx=0.04, rely=0.742, relheight=0.22, relwidth=0.2)
-    Add2 = tk.Frame(large_frame, bg='green', borderwidth=0, border=0)
+    main_frame = tk.Frame(Add2, bg='black', width=500, height=2000)
 
     global displayed
     displayed = 0
