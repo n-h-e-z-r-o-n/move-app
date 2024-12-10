9e91c92cb015b486ccaaacfbea914bf2caac879c
Hezron Na <hezron.w12@gmail.com>
2024-12-10 06:45:41 -0800

    Committed 2024 1Tu. 64541

--- Web App/script.js
+++ Web App/script.js
@@ -27,7 +27,7 @@ function start_slider(){
         }
 
         // Automatically change the image every 3 seconds
-        setInterval(showNextImage, 5000);
+        setInterval(showNextImage,1000);
 
         // Add click event to redirect when an active image is clicked
 
@@ -119,7 +119,7 @@ function Slider_Display(movies) {
   Slider_div.innerHTML = "";
   let count = 0 ;// Math.floor(Math.random() * (11 - 0) + 0);
 
-  let count_max = count + 12;
+  let count_max = count + 15;
   let start = 0;
   console.log(count);
   movies.forEach((movie, index) => {
