d9b45b6cba5aa13bfa679708f0d144d9e2057ef4
Hezron Na <hezron.w12@gmail.com>
2024-12-10 09:46:56 -0800

    Committed 2024 1Tu. 94656

--- Web App/script.js
+++ Web App/script.js
@@ -76,7 +76,7 @@ function start_slider(){
       const arrows = document.querySelectorAll(".arrow");
       const movieLists = document.querySelectorAll(".movie-list");
 
-      arrows.forEach((arrow, i) => {
+      arrowsRight.forEach((arrow, i) => {
         const itemNumber = movieLists[i].querySelectorAll("img").length;
         let clickCounter = 0;
 
